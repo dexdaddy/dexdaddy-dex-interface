202ab1f0e84b4264d06a0c0304dc4e40eb504449
dexdaddy <95766865+dexdaddy@users.noreply.github.com>
2022-05-05 14:23:32 +0530

    feat:changed tooken list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,6 +1,6 @@
 // the Pangolin Default token list lives here
-export const PANGOLIN_LIST = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/pangolin.tokenlist.json'
-export const WAGMI_LIST = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/wgm.tokenlist.json'
+export const PANGOLIN_LIST = 'https://raw.githubusercontent.com/dexdaddy/tokenlists/main/pangolin.tokenlist.json'
+export const WAGMI_LIST = 'https://raw.githubusercontent.com/dexdaddy/tokenlists/main/wgm.tokenlist.json'
 
 export const DEFAULT_TOKEN_LISTS_SELECTED: string[] = [PANGOLIN_LIST, WAGMI_LIST]
 
