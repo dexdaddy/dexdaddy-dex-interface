fef79d0a4e07dfa3e16b99515ab91cd90a3ad6cc
dexdaddy <95766865+dexdaddy@users.noreply.github.com>
2022-05-05 14:34:00 +0530

    test: deploy to prod

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -7,7 +7,7 @@ export const DEFAULT_TOKEN_LISTS_SELECTED: string[] = [PANGOLIN_LIST, WAGMI_LIST
 export const DEFAULT_TOKEN_LISTS: string[] = [PANGOLIN_LIST, WAGMI_LIST]
 
 // Token addresses (excluding PNG and WAVAX) ported from:
-// https://raw.githubusercontent.com/pangolindex/tokenlists/main/aeb.tokenlist.json
+// https://raw.githubusercontent.com/pangolindex/tokenlists/main/aeb.tokenlist.json //
 export const AEB_TOKENS: string[] = [
   '0xf20d962a6c8f70c731bd838a3a388D7d48fA6e15',
   '0xE54EB2C3009Fa411BF24fB017F9725b973CE36F0',
